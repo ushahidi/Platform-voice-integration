cd4673338205f4213120613c0f1f91631eea1a52
Jenniline <jennilineebai@gmail.com>
2019-08-13 23:07:43 +0100

    commiting the route file which is web.php

--- voice-integration_project/routes/web.php
+++ voice-integration_project/routes/web.php
@@ -1,16 +1,6 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It is a breeze. Simply tell Lumen the URIs it should respond to
-| and give it the Closure to call when that URI is requested.
-|
-*/
-
-$router->get('/', function () use ($router) {
-    return $router->app->version();
-});
+   namespace App\Http\Controllers;
+      
+   $router->get('/audio-transcribe',['uses' => 'audiofileController@parseAudio']);
+   
